b760cd5310e23121d6353517a9270bc4e4822823
alejandromango <amartinginnold@gmail.com>
2022-11-30 16:21:15 -0800

    Switched back to end detection

--- ArmMonitor.py
+++ ArmMonitor.py
@@ -12,7 +12,7 @@ parser.add_argument('port', type=str, help='serial port to be monitored')
 args = parser.parse_args()
 print("Opening port at ", args.port)
 ser = serial.Serial(args.port, 115200)  # open serial at the port given in the arguments
-arm = io.TextIOWrapper(io.BufferedReader(ser))
+arm = io.TextIOWrapper(io.BufferedReader(ser), newline='\n')
 
 monitor = numpy.array([])
 angleTimes = numpy.array([])
@@ -20,10 +20,10 @@ angles = numpy.array([])
 
 def endExperiment(data, times, log):
     angleData = pandas.DataFrame({'Time': times, 'Angle': data})
-    angleData.to_csv("AngleData.csv")
+    angleData.to_csv("AnalogAngleData.csv")
     print(monitor.shape)
     logData = pandas.DataFrame(monitor)
-    logData.to_csv("LogData.csv")
+    logData.to_csv("AnalogLogData.csv")
 while (True):
     time.sleep(0.001)
     while (True):
@@ -35,12 +35,12 @@ while (True):
             if (int(aTerms[1]) == 1):
                 print(aTerms[2])
                 monitor = numpy.append(monitor, aTerms)
-                # if ("Finished all movement cycles" in aTerms[2]) | ("Aborted Run" in aTerms[2]):
-                #     endExperiment(angles, angleTimes, monitor)
-                #     quit()
+                if ("Finished all movement cycles" in aTerms[2]) | ("Aborted Run" in aTerms[2]):
+                    endExperiment(angles, angleTimes, monitor)
+                    quit()
             elif (int(aTerms[1]) == 0):
                 angleTimes = numpy.append(angleTimes, float(aTerms[0]))
                 angles = numpy.append(angles, float(aTerms[2]))
-                if len(angles) > 5000:#("Finished all movement cycles" in aTerms[2]) | ("Aborted Run" in aTerms[2]):
-                    endExperiment(angles, angleTimes, monitor)
-                    quit()
+                # if len(angles) > 5000:#("Finished all movement cycles" in aTerms[2]) | ("Aborted Run" in aTerms[2]):
+                #     endExperiment(angles, angleTimes, monitor)
+                #     quit()
